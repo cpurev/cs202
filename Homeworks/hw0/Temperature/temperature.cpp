f6d309aa7a752ce76bbaa50243255d935d365e05
cpurev <cpurev@alaska.edu>
2020-01-16 10:00:01 -0900

    Prototypes

---
+++ Homeworks/hw0/Temperature/temperature.cpp
@@ -0,0 +1,21 @@
+//temperature.cpp
+//2020-1-18
+//Chuluunbat Purev
+//Fahrenheit to Celsius
+
+#include <iostream>
+
+double cpp_tfoc(const char* str) {
+
+}
+
+double c_ctof(const char* str) {
+
+}
+
+int main(int argc, char *argv[]) {
+
+
+
+	return 1;
+}
