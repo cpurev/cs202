8a7002bc6c619992894a00c499c31ac9876d8e80
Chuluunbat Purev <puluunbat@gmail.com>
2020-01-26 17:02:21 -0800

    Test run 3

--- Homeworks/hw1/Time It 1/main.cpp
+++ Homeworks/hw1/Time It 1/main.cpp
@@ -9,16 +9,37 @@
 #include <algorithm>
 #include <vector>
 
-void stopWatchTrials(std::vector<int>& v, int size) {
+void stopWatchTrials(std::vector<double>& v, int size, StopWatch a) {
+
+	v.clear();
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_int_distribution<> dis(1, 10);
 
-	for (auto i = 0; i < size; i++) {
+	a.Start();
+	for (auto i = 0; i < size - v.size(); i++) {
 		v.push_back(dis(gen));
 	}
+	a.Stop();
+	std::cout << "Pushing " << size << " items in to vector: " << a.eTimeSec() << std::endl;
 
+	int arr[] = { 7 };
+
+	a.Start();
+	std::binary_search(v.begin(), v.end(), 7);
+	a.Stop();
+	std::cout << "Binary search 7 in " << size << " items: " << a.eTimeSec() << std::endl;
+
+	a.Start();
+	std::search(v.begin(), v.end(), arr, arr + 1);
+	a.Stop();
+	std::cout << "Search 7 in "<< size << " items: " << a.eTimeSec() << std::endl;
+
+	a.Start();
+	std::find(v.begin(), v.end(), 7);
+	a.Stop();
+	std::cout << "Find 7 in " << size << " items: " << a.eTimeSec() << std::endl;
 
 
 }
@@ -26,31 +47,26 @@ void stopWatchTrials(std::vector<int>& v, int size) {
 int main() {
 
 	int n = 0;
-	std::vector<int> v;
-	int arr[] = { 7 };
-
+	std::vector<double> v;
 	StopWatch a;
 
+	stopWatchTrials(v, 10000, a);
 
-	std::binary_search(v.begin(), v.end(), 7);
-	a.Stop();
-	std::cout << "Binary search 7 in  100 items: " << a.eTimeSec() << std::endl;
+	std::cout << std::endl;
 
-	a.Start();
-	std::search(v.begin(), v.end(), arr, arr+1);
-	a.Stop();
-	std::cout << "Search 7 in 100 items: " << a.eTimeSec() << std::endl;
+	stopWatchTrials(v, 100000,a );
 
-	a.Start();
-	std::find(v.begin(), v.end(), 7);
-	a.Stop();
-	std::cout << "Find 7 in 100 items: " << a.eTimeSec() << std::endl;
+	std::cout << std::endl;
 
-	a.Start();
-	std::count(v.begin(), v.end(), 7);
-	a.Stop();
-	std::cout << "Count 7 in 100 items: " << a.eTimeSec() << std::endl;
+	stopWatchTrials(v, 1000000,a );
 
+	std::cout << std::endl;
+
+	stopWatchTrials(v, 10000000, a);
+
+	std::cout << std::endl;
+
+	stopWatchTrials(v, 100000000, a);
 
 	return 1;
 }
