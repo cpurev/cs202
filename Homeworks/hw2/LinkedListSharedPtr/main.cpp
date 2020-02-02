8372b9b396c862bfbbd8b194355ef02d35c2a777
Chuluunbat Purev <puluunbat@gmail.com>
2020-02-02 14:38:12 -0800

    Test for queue added

--- Homeworks/hw2/LinkedListSharedPtr/main.cpp
+++ Homeworks/hw2/LinkedListSharedPtr/main.cpp
@@ -10,11 +10,20 @@
 
 Colors Col[] = { "GREEN", "RED", "BLUE", "YELLOW", "BLACK", "WHITE" };
 std::list<std::shared_ptr<Colors>> list;
+Colors c = { "ORANGE" };
 
-TEST_CASE("QUEUE: FIRST IN FIRST OUT", "[queue]") {
+TEST_CASE("TESTING COLORS.HPP", "[colors]") {
 	for(auto x : Col)
 		addQueue(list, x);
-	REQUIRE(find(list, Col[2]) == true);
+
+	SECTION("QUEUE: FIRST IN FIRST OUT") {
+		addQueue(list, c);
+		REQUIRE(list.back()->str == c.str);
+
+		popQueue(list);
+		REQUIRE(list.front()->str != Col[0].str);
+
+	}
 }
 
 
