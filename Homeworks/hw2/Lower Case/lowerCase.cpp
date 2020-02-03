e6df6cd6fc086d2940d847761b15035164437dc4
Chuluunbat Purev <puluunbat@gmail.com>
2020-02-02 21:45:52 -0800

    Added for loop that changes the string into lowercase

--- Homeworks/hw2/Lower Case/lowerCase.cpp
+++ Homeworks/hw2/Lower Case/lowerCase.cpp
@@ -8,9 +8,13 @@
 int main() {
 
 	const char* c = "HeLlO WoRld!";
+	int a = 0;
 
-	for (auto i = 0; i < strlen(c); i++)
-		std::cout << c[i] << " ";
-
+	for (auto i = 0; i < strlen(c); i++) {
+		if (c[i] > 65 && c[i] < 90)
+			std::cout << (char)(c[i] + 32);
+		else
+			std::cout << c[i];
+	}
 	return 1;
 }
