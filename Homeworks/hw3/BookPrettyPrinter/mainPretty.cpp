b57d40a62657940def9ffa9b7be97ab8c6caefdc
Chuluunbat Purev <puluunbat@gmail.com>
2020-02-16 18:23:18 -0800

    Added command line arg checks

--- Homeworks/hw3/BookPrettyPrinter/mainPretty.cpp
+++ Homeworks/hw3/BookPrettyPrinter/mainPretty.cpp
@@ -5,20 +5,60 @@
 
 #include "simpleTokenizer.hpp"
 #include <iostream>
+#include <string>
+#include <algorithm>
+
+bool is_number(const std::string& s)
+{
+	return !s.empty() && std::find_if(s.begin(),
+		s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
 
 int main(int argc, char* argv[]) {
+
+	//Exit if no arguments
+	if (argc == 1)
+		return 0;
 
 	std::vector<std::string> paragraphs;
 	std::string line;
 
+	//Init file
 	std::ifstream file("test.txt");
-
 	if (!file) {
 		std::cout << "File error!";
 		return 0;
 	}
 
-	lineToTokens(line, paragraphs);
+	if(argv[2] != NULL)
+		line = argv[2];
+	else {
+		std::cout << "TO DO WHAT?";
+		return 0;
+	}
 
+	while (std::getline(file, line)) {
+		lineToTokens(line, paragraphs);
+	}
+
+	if (line == "--html")
+		goto html;
+	if (is_number(line))
+			goto wrapping;
+
+	int wrap;
+wrapping: {
+	for (auto i = 0; i < paragraphs.size(); i++) {
+		wrap = paragraphs[i].length() + 1;
+		if (wrap >= 40) {
+			i--;
+			continue;
+		}
+		std::cout << paragraphs[i] << " ";
+	}
+}
+html: {
+	
+}
 	return 1;
 }
