cb28e5b384dc7b4cd77bb1ca8dcbbbe1e999bfdc
Chuluunbat Purev <puluunbat@gmail.com>
2020-02-16 17:20:56 -0800

    Added final comments

--- Homeworks/hw3/Simple Tokenizer/mainTokenzier.cpp
+++ Homeworks/hw3/Simple Tokenizer/mainTokenzier.cpp
@@ -14,11 +14,14 @@
 //Lines to tokens
 bool lineToTokens(const std::string& line, std::vector<std::string>& tokens) {
 
+	//Save newline as blank line
 	if (line == "") {
 		tokens.push_back("blank line");
 		return true;
 	}
 
+	//Ignoring whitespaces
+	//Puting all the values in line to tokens
 	std::istringstream is(line);
 	std::string str;
 
@@ -30,42 +33,63 @@ bool lineToTokens(const std::string& line, std::vector<std::string>& tokens) {
 	return true;
 }
 
-bool readLine(std::istream& is, std::vector<std::string>& tokens, std::vector<std::pair<int, int>>& linecols) {
+//Reads line while also saving its position
+bool readLine(std::istream& is, std::vector<std::string>& tokens, 
+						std::vector<std::pair<int, int>>& linecols) {
 
+	//Init values
 	std::string line;
 	int cols, rows = 1;
 	std::string str;
 
+	//Loop for each line
 	while (std::getline(is, line)) {
+		
+		//Reset column number
 		cols = 1;
 
+		//If its new line put blank space and continue;
 		if (line == "" || line == "\n") {
 			tokens.push_back("blank line");
 			linecols.push_back(std::make_pair(rows, cols ));
+			continue;
 		}
+
+		//Iterate throught whole line character by character
 		for (auto l : line) {
 
+			//If its not white space add characters creating words which were together
 			if (l != ' ') {
 				str += l;
 
 			}
+			//If its white space
 			else {
+				//If there is a word before white space
+				//Push it to tokens and save the cols and rows
 				if (str.length() != 0) {
 					tokens.push_back(str);
+					//We minus lenght because we want to have words start position not end
 					linecols.push_back(std::make_pair(rows, cols- str.length()));
 				}
+				//Reset the string
 				str = "";
 			}
+			//Increase columns
 			++cols;
 
 		}
 
+		//This is for the time when we need to save the word before the newline
+		//New line skips the code above so we call this again
 		if (str.length() != 0) {
 			tokens.push_back(str);
 			linecols.push_back(std::make_pair(rows, cols - str.length()));
 		}
+		//Reset string
 		str = "";
 
+		//Increase rows
 		rows++;
 
 	}
@@ -77,20 +101,23 @@ bool readLine(std::istream& is, std::vector<std::string>& tokens, std::vector<st
 void printTokens(std::ostream& os, std::vector<std::string>& tokens, std::vector<std::pair<int, int>>& linecols) {
 
 	for (auto i = 0; i < tokens.size(); i++) {
-		std::cout << std::setw(5) << "Line " << linecols[i].first << ", Column " << linecols[i].second << ": \"" << tokens[i] << "\"\n";
+		std::cout << std::setw(5) << "Line " << linecols[i].first << ", Column " << 
+					linecols[i].second << ": \"" << tokens[i] << "\"\n";
 	}
 
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
 	StopWatch sw;
 
+	//If there is no arguments end;
 	if (argc == 1)
 		return 0;
 
 	std::ifstream file(argv[1]);
 
+	//Check file existance
 	if (!file) {
 		std::cout << "File error!";
 		return 0;
@@ -99,10 +126,19 @@ int main(int argc, char *argv[]) {
 	std::vector<std::string> tokens;
 	std::vector<std::pair<int, int>> linecols;
 
+	//Line number counter
 	int n = 0;
+
+	//Line saver which is also used for the command argument
+	//container so we can check in the if statement
+	//If there is no 2nd argument we go to notLineOnly
 	std::string line;
 	if (argv[2] != NULL)
 		line = argv[2];
+	else
+		goto notLineOnly;
+
+	//If the second argument is invalid we end the program
 	if (line == "--lineonly") {
 		line = "";
 		sw.Start();
@@ -111,12 +147,17 @@ int main(int argc, char *argv[]) {
 			++n;
 		}
 		sw.Stop();
-		std::cout << "Time it took to read" << n << " lines to tokens: " << sw.eTimeSec() << "s";
+		std::cout << "Time it took to read " << n << " lines to tokens: " << sw.eTimeSec() << "s";
 		return 1;
 	}
+	else
+		return 0;
 
+	//Time saver
 	double d;
 
+	//I put it in block so i can call goto on this
+notLineOnly:{
 	sw.Start();
 	readLine(file, tokens, linecols);
 	sw.Stop();
@@ -126,6 +167,6 @@ int main(int argc, char *argv[]) {
 	sw.Stop();
 
 	std::cout << std::endl << "Time it took to read and tokenize: " << d << "s\n" << "Time it took to print: " << sw.eTimeSec() << "s\n";
-
+}
 	return 1;
 }
