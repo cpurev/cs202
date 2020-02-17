fed6e2312439df1c862efd6a7ea647ae1726d819
Chuluunbat Purev <puluunbat@gmail.com>
2020-02-16 18:08:25 -0800

    File error check and reading

--- Homeworks/hw3/BookPrettyPrinter/mainPretty.cpp
+++ Homeworks/hw3/BookPrettyPrinter/mainPretty.cpp
@@ -3,11 +3,22 @@
 //Chuluunbat Purev
 //Pretty Format books
 
-#include <fstream>
+#include "simpleTokenizer.hpp"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 
+	std::vector<std::string> paragraphs;
+	std::string line;
 
+	std::ifstream file("test.txt");
+
+	if (!file) {
+		std::cout << "File error!";
+		return 0;
+	}
+
+	lineToTokens(line, paragraphs);
 
 	return 1;
 }
