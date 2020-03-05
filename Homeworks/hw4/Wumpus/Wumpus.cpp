bd64f0a6005066d3766c5be2e2a8a0c103a9357d
Chuluunbat Purev <puluunbat@gmail.com>
2020-03-04 18:09:41 -0800

    Created the files

---
+++ Homeworks/hw4/Wumpus/Wumpus.cpp
@@ -0,0 +1,11 @@
+//Wumpus.cpp
+//Chuluunbat Purev
+//2020-03-04
+//Hunt the Wumpus Game
+
+int main(void) {
+
+
+
+	return 1;
+}
---
+++ Homeworks/hw4/Wumpus/wumpusCave.hpp
@@ -0,0 +1,11 @@
+#ifndef WUMPUS_CAVE
+#define WUMPUS_CAVE
+
+class cave {
+public:
+	
+private:
+
+};
+
+#endif // !WUMPUS_CAVE
