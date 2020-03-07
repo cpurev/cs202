4ae4b2e8d751dc9feb6cb0afcdc18d37b4012eeb
Chuluunbat Purev <puluunbat@gmail.com>
2020-03-07 15:24:05 -0800

    Added factorial recursion and loop

--- Homeworks/hw4/RecursionProblems/Source.cpp
+++ Homeworks/hw4/RecursionProblems/Source.cpp
@@ -5,6 +5,23 @@
 
 #include <iostream>
 
+//Factorial with recursion
+unsigned int factorial(int n) {
+	return n <= 1 ? 1 : factorial(n - 1) * n;
+}
+
+//Factorial with loop
+unsigned int factorial_l(int n) {
+	int j = 1;
+	for (auto i = 1; i <= n; i++) {
+		j *= i;
+	}
+	return j;
+}
+
 int main(void) {
+
+	std::cout << factorial_l(0);
+
 	return 1;
 }
