f9ddf86530f6a66da1c088662e42df8c329224bd
Chuluunbat Purev <puluunbat@gmail.com>
2020-03-07 14:50:04 -0800

    Final comments

--- Homeworks/hw4/Wumpus/Wumpus.cpp
+++ Homeworks/hw4/Wumpus/Wumpus.cpp
@@ -6,26 +6,34 @@
 #include "wumpusCave.hpp"
 
 int main(void) {
-
+	//Init rooms
 	Cave cave;
 
+	//Init bat, wump, pit rooms randomly
 	cave.initRooms();
 
+	//Connect the rooms randomly
 	cave.connectRooms();
 
+	//Debugging print to caveLayout.txt file
 	cave.printRooms();
 
 	while (true) {
+		//State checker for the game results
 		if (!cave.playerState || !cave.wumpState)
 			break;
 
+		//Another state checker
 		if (cave.arrows == 0) {
 			std::cout << "You are out of arrows!\n";
 			cave.playerState = false;
 			break;
 		}
 
+		//Current room alert
 		std::cout << "You are in room: " << cave.getCurrentRoom() << '\n';
+
+		//Current room hazards
 		switch (cave.hazard())
 		{
 		case -1: std::cout << "A pit! Luckily bats were there too! They carried you to random room!\n";
@@ -37,14 +45,17 @@ int main(void) {
 			break;
 		}
 
+		//Adjacent room hazard printer
 		cave.hazardHint();
 
+		//Adjacent room sprinter
 		std::cout << "Tunnels lead to: " + cave.getAdjRooms();
 
 		char c;
 		int i = 0;
 		std::cout << "Shoot or Move? (S-M): "; std::cin >> c;
 
+		//Move the player and restart
 		if (c == 'M' || c == 'm') {
 			 do{
 				std::cout << "Where to? "; std::cin >> i;
@@ -53,6 +64,8 @@ int main(void) {
 			continue;
 		}
 
+		//Shoot
+		//From the results checks if to restart or to end game
 		if (c == 'S' || c == 's') {
 			std::cout << "You have #" << cave.arrows << " arrows\n";
 			std::cout << "Enter a room to shoot at: "; std::cin >> i;
@@ -64,9 +77,11 @@ int main(void) {
 
 	}
 
+	//PLayer loss
 	if (cave.playerState == false)
 		std::cout << "Game Over!";
 
+	//PLayer win
 	if (!cave.wumpState)
 		std::cout << "You killed the wumpus! You had " << cave.arrows << " arrows left!";
 
