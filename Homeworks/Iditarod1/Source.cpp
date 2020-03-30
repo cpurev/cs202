a26e8201f73aa77e98cab0eff07ae2cfce78e30f
Chuluunbat Purev <puluunbat@gmail.com>
2020-03-30 12:20:46 -0700

    Wrote fibonacci with recursion

---
+++ Homeworks/Iditarod1/Source.cpp
@@ -0,0 +1,18 @@
+//Source.cpp
+//Chuluunbat Purev
+//2020-3-30
+//Fibonnaci and Factorial
+
+#include <iostream>
+
+//Recursive fibonacci
+unsigned int fibonacci(int n) {
+	return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(void) {
+
+
+
+	return 1;
+}
