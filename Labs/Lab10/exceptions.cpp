81e3ceaabd129e672bfc977d415bab7a7a099629
Chuluunbat Purev <puluunbat@gmail.com>
2020-04-01 12:11:55 -0700

    Throws and does try/catch

---
+++ Labs/Lab10/exceptions.cpp
@@ -0,0 +1,43 @@
+//Lab10 - exceptions.cpp
+//Chuluunbat Purev
+//2020-4-1
+//Exception usage
+
+#include <iostream>
+
+void functionC()
+{
+	//your code here
+	throw std::runtime_error("functionC() threw");
+}
+
+void functionB()
+{
+	//your code here
+	std::cout << "Starting functionB()\n";
+	functionC();
+	std::cout << "Ending functionB()\n";
+}
+
+void functionA()
+{
+	// your code here
+	try{
+		functionB();
+	}
+	// your code here
+
+	catch (const std::exception e) {
+		std::cout << "Caught an exception: " << e.what() << std::endl;
+	}
+	
+	// your code here
+}
+
+int main()
+{
+	std::cout << "Starting main()" << std::endl;
+	functionA();
+	std::cout << "Ended normally." << std::endl;
+	return 0;
+}
