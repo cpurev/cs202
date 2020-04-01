9c833083ea060f6818dcf1a4d3cd130a49565d33
Chuluunbat Purev <puluunbat@gmail.com>
2020-04-01 11:23:50 -0700

    Static member fucntion

--- Labs/Lab9/Source.cpp
+++ Labs/Lab9/Source.cpp
@@ -7,7 +7,7 @@
 
 class staticClass {
 public:
-	staticClass() {
+	staticClass() : dummy(0){
 	}
 	void count() {
 		++_count;
@@ -18,13 +18,18 @@ public:
 	static void resetCount() {
 		_count = 0;
 		std::cout << __FUNCTION__ << " " << _count << std::endl;
+
+		//Dummy is not static so this is wrong
+		//std::cout << __FUNCTION__ << " dummy " << dummy << std::endl;
 	}
 private:
+	int dummy;
 	static int _count;
 };
 
 int staticClass::_count = 5;
 
+//count is local static variable
 void staticLocal() {
 	static int count;
 
@@ -34,19 +39,33 @@ void staticLocal() {
 }
 
 int main() {
-		
+	
+	std::cout << "This is local static\n";
+	//Count keeps increasing because its static
 	staticLocal();
 	staticLocal();
 	staticLocal();
 
+	std::cout << "\nThis is object a\n";
+	//This count is only in class and only the class can use it
+	//Since its static all across the same class object variable is the same
 	staticClass a;
 	a.count();
 	a.count();
 	a.count();
 
-	a.resetCount();
+	std::cout << "\nThis is object b\n";
+	staticClass b;
+	b.count();
+
+
+	std::cout << "\nThis is static function reseting count\n";
+	//resetCount static function it will reset all the other static counts
+	staticClass::resetCount();
+	std::cout << "\nThis is object a\n";
 	a.count();
-	a.count();
+	std::cout << "\nThis is object b\n";
+	b.count();
 
 	return 1;
 }
