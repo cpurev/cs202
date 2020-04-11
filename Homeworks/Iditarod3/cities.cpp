08fce590deeb20c5b149592ddfbabd7b51da1a29
Chuluunbat Purev <puluunbat@gmail.com>
2020-04-11 09:40:45 -0700

    Distance method checks indexes

--- Homeworks/Iditarod3/cities.cpp
+++ Homeworks/Iditarod3/cities.cpp
@@ -29,31 +29,39 @@ void CityList::load (const std::string& str){
 
 	std::ifstream input(str);
 
+	//Check if file is open
 	if (!input) {
 		std::cout << "File error";
 		return;
 	}
 
+	//Loop till node cord section
 	std::string a;
 	while (std::getline(input, a)) {
 		if (a == "NODE_COORD_SECTION")
 			break;
 	}
 
+	//Starting here are the coords
 	int num;
 	double d;
 	while (std::getline(input, a)) {
+		//EOF breaker
 		if (a == "EOF")
 			break;
 		CityNode cn;
 		std::istringstream iss(a);
+		//First number is id
 		iss >> num;
 		cn.id(num);
+		//Second number is x
 		iss >> d;
 		cn.x(d);
+		//Third number is y
 		iss >> d;
 		cn.y(d);
 
+		//Push it back to the vector
 		cityList.push_back(std::make_shared<CityNode>(cn));
 			
 	}
@@ -65,7 +73,10 @@ void CityList::print() const {
 }
 
 double CityList::distance(int first, int second) const {
+	//If both nodes are the same
 	if (first == second) return 0;
+	//If nodes are out of range
 	if (cityList.size() <= first || cityList.size() <= second) return 0;
+	//Calculate
 	return std::sqrt(std::pow(cityList[second]->x() - cityList[first]->x(), 2) + std::pow(cityList[second]->y() - cityList[first]->y(), 2));
 }
