d81706c2269b8662574af4ac39964da4f02e8433
Chuluunbat Purev <puluunbat@gmail.com>
2020-04-11 09:53:29 -0700

    Final print prompts

--- Homeworks/Iditarod3/Source.cpp
+++ Homeworks/Iditarod3/Source.cpp
@@ -8,13 +8,22 @@
 
 int main(void) {
 
-	CityList cl;
-	cl.load("vm1748.tsp");
-	cl.print();
+	CityList vm;
+	vm.load("vm1748.tsp");
+	CityList d15;
+	d15.load("d15112.tsp");
+	CityList d18;
+	d18.load("d18512.tsp");
+	CityList rl;
+	rl.load("rl5934.tsp");
+	CityList us;
+	us.load("usa13509.tsp");
 
+
+	vm.print();
 
 	std::cout << "Distance from node 1 to 2" << std::endl;
-	std::cout << cl.distance(1, 2);
+	std::cout << vm.distance(1, 2);
 
 	return 0;
 }
