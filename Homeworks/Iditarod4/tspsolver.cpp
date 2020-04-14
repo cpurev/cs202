f93a29db1f83845d428d913028969697a7e2f1c2
Chuluunbat Purev <puluunbat@gmail.com>
2020-04-13 22:51:03 -0700

    Comments for clarification

--- Homeworks/Iditarod4/tspsolver.cpp
+++ Homeworks/Iditarod4/tspsolver.cpp
@@ -27,16 +27,19 @@ double TspSolver::solveRandomly(const CityList& l) {
 
 	//Random starting city
 	int start = randomInt(0, l.size() - 1);
-	chosen.addPath(start);
 
+	//Put randomly chosen city to list
+	//And delete it from unchosen
+	chosen.addPath(start);
 	unchosen.deletePath(start);
 
 	//Randomly connect cities
 	while (unchosen.size() != 0) {
+		//Random city which cannot repeat since
+		//It gets deleted away from unchosen
 		start = randomInt(0, unchosen.size()-1);
 
 		chosen.addPath(unchosen.getPath(start));
-
 		unchosen.deletePath(start);
 
 	}
@@ -72,22 +75,38 @@ double TspSolver::solveGreedy(const CityList& l) {
 	//Random starting city
 	int start = randomInt(0, l.size() - 1);
 
+	//Put randomly chosen city to list
+	//And delete it from unchosen
 	chosen.addPath(start);
 	unchosen.deletePath(start);
 
+	//Temp variables
 	int small, erase;
 
+	//Loop for N times or the total amount of nodes
 	for (auto i = 0; i < l.size(); i++) {
+		//We are finding the minimum so it can be whatever max number
+		//That will not be reached
 		dist = 1e12;
+
+		//If only 1 city is left then break
 		if (unchosen.size() == 1) {
 			chosen.addPath(unchosen.getPath(0));
 			unchosen.deletePath(0);
 			break;
 		}
+
+		//i is the last city we chose
+		//we repeated the amount of unchosen cities
+		//to find its closest city
 		for (auto j = 0; j < unchosen.size(); j++) {
 			if (dist > l.distance(chosen.getPath(i), unchosen.getPath(j))) {
 				dist = l.distance(chosen.getPath(i), unchosen.getPath(j));
+
+				//This is the closest city 
 				small = unchosen.getPath(j);
+
+				//This is the index to delete the found city not the city
 				erase = j;
 			}
 		}
@@ -99,6 +118,8 @@ double TspSolver::solveGreedy(const CityList& l) {
 	//First and last city are the same
 	chosen.addPath(chosen.getPath(0));
 
+	//Reset dist
+	//Keeping variables minimum and effiecent
 	dist = 0;
 
 	//Calculate total distance of this path
@@ -120,13 +141,18 @@ double TspSolver::solveMyWay(const CityList& l) {
 	//Init paths
 	CityPath chosen;
 
+	//Vector of x coordinates of the node
 	std::vector<double> x;
 	for (auto i = 0; i < l.size(); i++) {
 		x.push_back(l.node(i)->x());
 	}
 
+	//Sort them
 	std::sort(x.begin(), x.end());
 
+	//This loop is to find what are the sorted x
+	//values nodes are.
+	//And then put them in chosen citypath
 	for (auto i = 0; i < x.size(); i++) {
 		for (auto j = 0; j < l.size(); j++) {
 			if (x[i] == l.node(j)->x()) {
