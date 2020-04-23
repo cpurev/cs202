08d7916a3c10e2278ff9c2fb0adafe08365088fd
Chuluunbat Purev <puluunbat@gmail.com>
2020-04-23 11:26:34 -0700

    twice with c style string

--- Labs/Lab14/Source.cpp
+++ Labs/Lab14/Source.cpp
@@ -10,11 +10,18 @@ T twice(const T& t) {
 	return t + t;
 }
 
+std::string twice(const char* t) {
+	return twice(std::string(t));
+}
+
 int main(void) {
 	
 	std::cout << twice(2) << std::endl;
 	std::cout << twice(2.3) << std::endl;
 	std::cout << twice(std::string("Hello")) << std::endl;
 	
+
+	std::cout << twice("World") << std::endl;
+
 	return 0;
 }
